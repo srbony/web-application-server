997ffd22be396f2fa48e1b03b621bc73d3a685c0
srbony <srabonycse@gmail.com>
2022-12-29 12:04:02 +0600

    added create api

--- index.js
+++ index.js
@@ -13,17 +13,24 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvrwoc0.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+// console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         const Posts = client.db('socialMedia').collection('posts');
+        const About = client.db('socialMedia').collection('about');
         app.post('/post', async (req, res) => {
             const post = req.body;
             // console.log(post);
             const result = await Posts.insertOne(post);
             res.send(result);
         });
+        app.post('/about', async (req, res) => {
+            const about = req.body;
+            console.log(about);
+            const result = await About.insertOne(about);
+            res.send(result);
+        })
 
         app.get('/allPosts', async (req, res) => {
             const query = {};
